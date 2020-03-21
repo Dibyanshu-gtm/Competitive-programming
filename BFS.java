147d0a2829d27a89cc0204dd9932845267154e20
Dibyanshu Gautam <48611583+Dibyanshu-gtm@users.noreply.github.com>
2020-03-21 12:33:44 +0530

    Breadth First Search In Graphs JAVA

---
+++ BFS.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Main {
+    
+    
+    
+    int V;
+    LinkedList<Integer>adj[];
+    
+    Main(int v)
+    {
+        V=v;
+        adj=new LinkedList[v];
+        for(int i=0;i<v;i++)
+        {
+            adj[i]=new LinkedList();
+        }
+    }
+    
+    
+    void addEdge(int v,int w)
+    {
+        adj[v].add(w);
+    }
+    
+    void BFS(int s)
+    {
+        
+        boolean visited[]=new boolean[V];
+        LinkedList<Integer>q=new LinkedList<Integer>();
+        q.add(s);
+        visited[s]=true;
+        while(q.size()!=0)
+        {
+            s=q.poll();
+            System.out.print(s+" ");
+            
+            Iterator<Integer>it=adj[s].listIterator();
+            while(it.hasNext())
+            {
+                int n=it.next();
+                if(!visited[n])
+                {
+                    visited[n]=true;
+                    q.add(n);
+                }
+            }
+            
+            
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        
+        Main g=new Main(4);
+        g.addEdge(0, 1); 
+        g.addEdge(0, 2); 
+        g.addEdge(1, 2); 
+        g.addEdge(2, 0); 
+        g.addEdge(2, 3); 
+        g.addEdge(3, 3); 
+        
+        
+        g.BFS(2);
+        System.out.println("XXXXXXXX");
+    }
+}
